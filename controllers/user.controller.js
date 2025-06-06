618ae18a9fc6216ad912b2efb27630c48e55783f
karanwagh17 <kp76985566@gmail.com>
2025-06-06 15:13:15 +0530

    create

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
 const UserModel = require("../model/user.model");
-
+const bcrypt = require("bcrypt");
 const user = {
   register: async (req, res) => {
     const { name, email, password, age, city } = req.body;
@@ -11,16 +11,28 @@ const user = {
       return res.status(400).json({ message: "All fields are required" });
     }
     if (password.length < 8) {
-      return res.status(400).json({ message: "Password must be at least 8 characters long" });
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters long" });
     }
     if (!uppercaseRegex.test(password)) {
-      return res.status(400).json({ message: "Password must contain at least one uppercase letter" });
+      return res.status(400).json({
+        message: "Password must contain at least one uppercase letter",
+      });
     }
     if (!numberRegex.test(password)) {
-      return res.status(400).json({ message: "Password must contain at least one number" });
+      return res
+        .status(400)
+        .json({ message: "Password must contain at least one number" });
     }
     if (!specialCharRegex.test(password)) {
-      return res.status(400).json({ message: "Password must contain at least one special character" });
+      return res.status(400).json({
+        message: "Password must contain at least one special character",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    if (!hashedPassword) {
+        return res.status(500).json({ message: "Error hashing password" });
     }
     try {
       const existingUser = await UserModel.findOne({ email });
@@ -30,7 +42,7 @@ const user = {
       const user = await UserModel.create({
         name,
         email,
-        password,
+        password:hashedPassword,
         age,
         city,
       });
@@ -40,6 +52,30 @@ const user = {
       res.status(500).json({ message: "Internal server error" });
     }
   },
-  
+  login: async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+    try {
+      const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "create an account first" });
+        }
+        if (user.password !== password) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+
+        
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
 };
 module.exports = user;
