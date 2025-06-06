3204db33d1550664571bea81c04be6647fe49564
karanwagh17 <kp76985566@gmail.com>
2025-06-06 15:42:42 +0530

    create

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,25 +3,30 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const ejs = require("ejs");
 const email = require("../utils/email");
+
 const user = {
   register: async (req, res) => {
     const { name, email, password, age, city } = req.body;
-    const uppercaseRegex = /[A-Z]/;
-    const numberRegex = /[0-9]/;
-    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
 
     if (!name || !email || !password || !age || !city) {
       return res.status(400).json({ message: "All fields are required" });
     }
+
+    const uppercaseRegex = /[A-Z]/;
+    const numberRegex = /[0-9]/;
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
     if (password.length < 8) {
       return res
         .status(400)
         .json({ message: "Password must be at least 8 characters long" });
     }
     if (!uppercaseRegex.test(password)) {
-      return res.status(400).json({
-        message: "Password must contain at least one uppercase letter",
-      });
+      return res
+        .status(400)
+        .json({
+          message: "Password must contain at least one uppercase letter",
+        });
     }
     if (!numberRegex.test(password)) {
       return res
@@ -29,20 +34,21 @@ const user = {
         .json({ message: "Password must contain at least one number" });
     }
     if (!specialCharRegex.test(password)) {
-      return res.status(400).json({
-        message: "Password must contain at least one special character",
-      });
+      return res
+        .status(400)
+        .json({
+          message: "Password must contain at least one special character",
+        });
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    if (!hashedPassword) {
-      return res.status(500).json({ message: "Error hashing password" });
-    }
+
     try {
       const existingUser = await UserModel.findOne({ email });
       if (existingUser) {
         return res.status(400).json({ message: "User already exists" });
       }
-      const user = await UserModel.create({
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await UserModel.create({
         name,
         email,
         password: hashedPassword,
@@ -52,9 +58,12 @@ const user = {
 
       res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
-      res.status(500).json({ message: "Internal server error" });
+      res
+        .status(500)
+        .json({ message: error.message || "Internal server error" });
     }
   },
+
   login: async (req, res) => {
     const { email, password } = req.body;
 
@@ -63,27 +72,31 @@ const user = {
         .status(400)
         .json({ message: "Email and password are required" });
     }
+
     try {
       const user = await UserModel.findOne({ email });
       if (!user) {
-        return res.status(400).json({ message: "create an account first" });
+        return res.status(400).json({ message: "Create an account first" });
       }
-      if (user.password !== password) {
+
+      const passMatch = await bcrypt.compare(password, user.password);
+      if (!passMatch) {
         return res.status(400).json({ message: "Invalid password" });
       }
-      const pass = await bcrypt.compare(password, user.password);
-      if (!pass) {
-        return res.status(400).json({ message: "Invalid password" });
-      }
-      const token = await jwt.sign(
-        { userId: user._id },
-        process.env.JWT_SECRET,
+
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7m",
+      });
+
+      const htmltemplate = await ejs.renderFile(
+        __dirname + "/../views/conform.ejs",
         {
-          expiresIn: "7m",
+          name: user.name,
         }
       );
+      await email(user.email, htmltemplate, "Confirmation Message");
       res
-        .cookie("token", "user logged in", {
+        .cookie("token", token, {
           httpOnly: true,
           secure: true,
           sameSite: "Strict",
@@ -98,19 +111,11 @@ const user = {
             city: user.city,
           },
         });
-      const htmltemplate = await ejs.renderFile(
-        __dirname + "/../views/conform.ejs",
-        {
-          name: user.name,
-        }
-      );
-
-      await email(userData.email, htmltemplate, " conformationn message");
-      return res.status(200).json({ message: "accoun created" });
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
   },
+
   logout: async (req, res) => {
     try {
       res
@@ -122,4 +127,5 @@ const user = {
     }
   },
 };
+
 module.exports = user;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,5 +3,7 @@ const user = require("../controllers/user.controller");
 
 const userRouter = express.Router();
 userRouter.post("/register", user.register);
+userRouter.post("/login", user.login);
+userRouter.get("/logout", user.logout);
 
 module.exports = userRouter;
